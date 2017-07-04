7459ac895267ab04495c7b9bce15ac4166b7e2b0
Katherine <katherinezyb@brandeis.edu>
2017-07-04 00:59:41 -0400

    addRecipe page

---
+++ page/addRecipe/addRecipe.js
@@ -0,0 +1,25 @@
+Template.askforrecipe.events({
+  'click button'(elt,instance){
+    const dishName = instance.$('#dishName').val();
+    const recipe = instance.$('#recipe').val();
+
+    var dish = {
+      dishName:dishName,
+      recipe:recipe,
+      owner:Meteor.userId()
+    };
+    Meteor.call('dish.insert',dish);
+
+    instance.$('#dishName').val("");
+    instance.$('#recipe').val("");
+  }
+})
+
+Template.showRecipe.helpers({
+  recipeData() {return MyRecipe.find()}
+})
+
+Template.recipeRow.helpers({
+  isOwner() {console.dir(this);
+    return this.recipe.owner == Meteor.userId()}
+})
