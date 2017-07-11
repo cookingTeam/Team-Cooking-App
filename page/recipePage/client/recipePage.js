f469e8d09ca3576640b499d5b32a30d0c7e41372
hebuer <zhuoyuan@brandeis.edu>
2017-07-11 11:42:12 -0400

    fixed conflict

--- page/recipePage/client/recipePage.js
+++ page/recipePage/client/recipePage.js
@@ -1,6 +1,48 @@
-Template.ingredients.helpers({
-  ingredientList: function(){
-    // this should be a findOne for the recipe.id but I don't really know how that'd work
-    return Recipe.find();
+Template.recipePage.onCreated(
+  function(){
+    // dict = new ReactiveDict();
+    // dict.setDefault({
+    //     d: {}
+    //   });
+    Session.set("dict", {})
+    console.log(Session.get("dict"));
+    console.log(this)
+    Meteor.apply("getInstruction",[this.data], {returnStubValue:true},
+          function(error,result){
+            console.dir(['getInstruction',error,result]);
+            if (error) {
+              console.log("Error!!"+JSON.stringify(error)); return;
+            }
+            console.log(result);
+            r = JSON.parse(result);
+            // console.log("r[0].steps[0].step: "+r[0].steps[0].step);
+            //console.log(dict);
+              console.log("r  "+r[0]);
+            return Session.set("dict",r[0]);
+            //return r[0];
+            }
+          //.steps[0].step
+    );
+  }
+)
+
+Template.recipePage.helpers({
+  recipe: function(){
+    console.log("session:     " + Session.get("dict"));
+    return Session.get("dict");
+  }
+})
+
+Template.recipePage.events({
+  "click #popup_button": function(){
+    $("#popup1").css("visibility", "visible");
+    $("#popup1").css("opacity", 1);
+  }
+})
+
+Template.recipePage.events({
+  "click #popup_close":function(){
+    $("#popup1").css("visibility","hidden");
+    $('#popup1').css("opacity",0);
   }
 })
