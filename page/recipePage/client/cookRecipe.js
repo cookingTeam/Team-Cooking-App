7773c02f9300d3281a45aae53161f21aa677e943
hebuer <zhuoyuan@brandeis.edu>
2017-07-13 10:50:18 -0400

    add api.ai into auto speech

--- page/recipePage/client/cookRecipe.js
+++ page/recipePage/client/cookRecipe.js
@@ -21,18 +21,127 @@ Template.cookRecipe.events({
               result = event.results[i];
               if (result.isFinal) {
                 final_result = result[0].transcript;
-                  console.log(final_result);
-                console.log(final_result===('alexa'));
-                if(final_result.includes('alexa')){
-                  console.log("alexa is here");
-                  break;
-                }
-                console.log('SPEECH RECOGNITION : final transcript = ' + final_result+ "type: "+(typeof interim_result) , e);
+                // console.log(final_result);
+                console.log('SPEECH RECOGNITION : final transcript = ' + final_result+ "  type: "+(typeof interim_result) , e);
                 // trigger a command matching the final utterance here
               } else {
                 interim_result += result[0].transcript;
-                // console.log(interim_result);
+                if(result[0].transcript.includes('Alexa')){
+                  console.log("Alexa is here");
+                  // This is our accessToken to our group's account
+                  var accessToken = "6a670d47c5ba447facf2684bd9a3c0ee";
+                  var baseUrl = "https://api.api.ai/v1/";
+                  // this is if you want to type into the input textbox rather than speak
+                  // $(document).ready(function() {
+                  //
+                  //     // $("#input").keypress(function(event) {
+                  //     //   if (event.which == 13) {
+                  //     //     event.preventDefault();
+                  //     //     send();
+                  //     //   }
+                  //     // });
+                  //
+                  // });
+                  $("#rec").click(function(event) {
+                        switchRecognition();
+                  });
 
+                  //creating speech recognition functions
+                  var recognition;
+                  function startRecognition() {
+                    recognition = new webkitSpeechRecognition();
+                    recognition.onstart = function(event) {
+                      updateRec();
+                    };
+                    recognition.onresult = function(event) {
+                      var text = "";
+                        for (var i = event.resultIndex; i < event.results.length; ++i) {
+                          text += event.results[i][0].transcript;
+                        }
+                        setInput(text);
+                      stopRecognition();
+                    };
+                    recognition.onend = function() {
+                      stopRecognition();
+                    };
+                    recognition.lang = "en-US";
+                    recognition.start();
+                  }
+
+                  function stopRecognition() {
+                    if (recognition) {
+                      recognition.stop();
+                      recognition = null;
+                    }
+                    updateRec();
+                  }
+                  function switchRecognition() {
+                    if (recognition) {
+                      stopRecognition();
+                    } else {
+                      startRecognition();
+                    }
+                  }
+                  //this just prints the utterance in the textbar
+                  function setInput(text) {
+                    $("#input").val(text);
+                    action();
+                  }
+                  function updateRec() {
+                    $("#rec").text(recognition ? "Stop" : "Speak");
+                  }
+                  // function send() {
+                  //   var text = $("#input").val();
+                  //   $.ajax({
+                  //     type: "POST",
+                  //     url: baseUrl + "query?v=20150910",
+                  //     contentType: "application/json; charset=utf-8",
+                  //     dataType: "json",
+                  //     headers: {
+                  //       "Authorization": "Bearer " + accessToken
+                  //     },
+                  //     data: JSON.stringify({ query: text, lang: "en", sessionId: "somerandomthing" }),
+                  //     success: function(data) {
+                  //       console.log(data);
+                  //       setResponse(JSON.stringify(data, undefined, 2));
+                  //     },
+                  //     error: function() {
+                  //       setResponse("Internal Server Error");
+                  //     }
+                  //   });
+                  //   setResponse("Loading...");
+                  // }
+                  // function setResponse(val) {
+                  //   $("#response").text(val);
+                  // }
+
+                  function action() {
+                    var text = $("#input").val();
+                    $.ajax({
+                      type: "POST",
+                      url: baseUrl + "query?v=20150910",
+                      contentType: "application/json; charset=utf-8",
+                      dataType: "json",
+                      headers: {
+                        "Authorization": "Bearer " + accessToken
+                      },
+                      data: JSON.stringify({ query: text, lang: "en", sessionId: "somerandomthing" }),
+                      success: function(data) {
+                        if (data.result.action=='next_step'){
+                            console.log('success');
+                            //i++, write function nextStep(recipe.steps[i]) and call it here
+                        } else if (data.result.action=='stop'){
+                            recognition_engine.stop();
+                            console.log('creepy thing has stopped')
+                        }
+                        console.log(data);
+                      },
+                    });
+                  }
+
+
+
+                }
                 if(result[0].transcript.includes('stop')){
                   console.log("record stopped");
                   recognition_engine.stop();
@@ -40,119 +149,5 @@ Template.cookRecipe.events({
               }
             }
           };
-
-
-
-        // This is our accessToken to our group's account
-        var accessToken = "6a670d47c5ba447facf2684bd9a3c0ee";
-        var baseUrl = "https://api.api.ai/v1/";
-        // this is if you want to type into the input textbox rather than speak
-        // $(document).ready(function() {
-        //
-        //     // $("#input").keypress(function(event) {
-        //     //   if (event.which == 13) {
-        //     //     event.preventDefault();
-        //     //     send();
-        //     //   }
-        //     // });
-        //
-        // });
-        $("#rec").click(function(event) {
-              switchRecognition();
-        });
-
-        //creating speech recognition functions
-        var recognition;
-        function startRecognition() {
-          recognition = new webkitSpeechRecognition();
-          recognition.onstart = function(event) {
-            updateRec();
-          };
-          recognition.onresult = function(event) {
-            var text = "";
-              for (var i = event.resultIndex; i < event.results.length; ++i) {
-                text += event.results[i][0].transcript;
-              }
-              setInput(text);
-            stopRecognition();
-          };
-          recognition.onend = function() {
-            stopRecognition();
-          };
-          recognition.lang = "en-US";
-          recognition.start();
         }
-
-        function stopRecognition() {
-          if (recognition) {
-            recognition.stop();
-            recognition = null;
-          }
-          updateRec();
-        }
-        function switchRecognition() {
-          if (recognition) {
-            stopRecognition();
-          } else {
-            startRecognition();
-          }
-        }
-        //this just prints the utterance in the textbar
-        function setInput(text) {
-          $("#input").val(text);
-          action();
-        }
-        function updateRec() {
-          $("#rec").text(recognition ? "Stop" : "Speak");
-        }
-        // function send() {
-        //   var text = $("#input").val();
-        //   $.ajax({
-        //     type: "POST",
-        //     url: baseUrl + "query?v=20150910",
-        //     contentType: "application/json; charset=utf-8",
-        //     dataType: "json",
-        //     headers: {
-        //       "Authorization": "Bearer " + accessToken
-        //     },
-        //     data: JSON.stringify({ query: text, lang: "en", sessionId: "somerandomthing" }),
-        //     success: function(data) {
-        //       console.log(data);
-        //       setResponse(JSON.stringify(data, undefined, 2));
-        //     },
-        //     error: function() {
-        //       setResponse("Internal Server Error");
-        //     }
-        //   });
-        //   setResponse("Loading...");
-        // }
-        // function setResponse(val) {
-        //   $("#response").text(val);
-        // }
-
-        function action() {
-          var text = $("#input").val();
-          $.ajax({
-            type: "POST",
-            url: baseUrl + "query?v=20150910",
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            headers: {
-              "Authorization": "Bearer " + accessToken
-            },
-            data: JSON.stringify({ query: text, lang: "en", sessionId: "somerandomthing" }),
-            success: function(data) {
-              if (data.result.action=='next_step'){
-                  console.log('success');
-                  //i++, write function nextStep(recipe.steps[i]) and call it here
-              } else if (data.result.action=='stop'){
-                  recognition_engine.stop();
-                  console.log('creepy thing has stopped')
-              }
-              console.log(data);
-            },
-          });
-        }
-  }
-
-})
+      })
