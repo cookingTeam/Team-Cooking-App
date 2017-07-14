f1f71e57ed50a955d774e7b67c1a4403b7db284f
ww199766 <weiweiwei@brandeis.edu>
2017-07-14 15:30:31 -0400

    keypress "enter" && return to search result

--- page/recipe_search/client/recipeSearch.js
+++ page/recipe_search/client/recipeSearch.js
@@ -7,8 +7,17 @@ Template.recipeSearch.onCreated(
     }
 )
 
+Template.recipeSearch.onRendered(function(){
+  console.log(Router.current().params.query.keyword);
+  if(Router.current().params.query.keyword){
+    Template.instance().$('#input').val(Router.current().params.query.keyword);
+    Template.instance().$('#search').click();
+  }
+})
+
 Template.recipeSearch.events({
   "click #search": function(elt, instance){
+
       const search_param_dish = instance.$('#input').val();
       Meteor.apply("getRecipe",[search_param_dish],{returnStubValue: true},
         function(error,result){
@@ -18,11 +27,20 @@ Template.recipeSearch.events({
           }
           console.log(result);
           r = JSON.parse(result);
-          console.dir(r);
+          //console.dir(r);
           return instance.state.set("recipes",r.results);
         });
-      }
-   })
+
+      var query = 'keyword='+search_param_dish;
+      Router.go("recipeSearch", {}, {query: query});
+    },
+
+  'keypress #input': function(elt, instance){
+    if(event.which==13){
+      instance.$('#search').click();
+    }
+  }
+})
 
 Template.res.events({
       "click .clickable-row": function(event,instance){
