462cfb5b130bc5bc99f373aca9bb0e0399f3da91
mmarcus11 <mmarcus@brandeis.edu>
2017-07-17 10:04:51 -0400

    adding dynamic adding texbox

--- page/addRecipe/client/addRecipe.js
+++ page/addRecipe/client/addRecipe.js
@@ -12,8 +12,26 @@ Template.askforrecipe.events({
 
     instance.$('#dishName').val("");
     instance.$('#steps').val("");
+  },
+
+  'click #addIngredient': function(){
+      var container = document.getElementById("container");
+      container.appendChild(document.createTextNode("Ingredient "));
+      var input = document.createElement("input");
+      input.type = "text";
+      container.appendChild(input);
+      container.appendChild(document.createElement("br"));
+  },
+
+  'click #addStep': function(){
+      var container = document.getElementById("stepscontainer");
+      container.appendChild(document.createTextNode("Step "));
+      var input = document.createElement("textarea");
+      container.appendChild(input);
+      container.appendChild(document.createElement("br"));
   }
 })
+
 
 Template.showRecipe.helpers({
   recipeData() {return MyRecipe.find()}
