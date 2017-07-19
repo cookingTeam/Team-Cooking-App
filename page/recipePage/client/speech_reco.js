ba8881623ca0227a16388f400493282cae6616b6
mmarcus11 <mmarcus@brandeis.edu>
2017-07-19 17:52:01 -0400

    fixing voice reco placement.

--- page/recipePage/client/speech_reco.js
+++ page/recipePage/client/speech_reco.js
@@ -22,116 +22,116 @@ Template.slide.onCreated(function(){
                 // console.log(final_result);
                 console.log('SPEECH RECOGNITION : final transcript = ' + final_result, e);
                 // trigger a command matching the final utterance here
-                if(final_result.includes('stop')){
-                  console.log("record stopped");
-                  recognition_engine.stop();
-                }
-                if(final_result.includes('cookie')){
-                  // recognition_engine.stop();
-                  console.log("Alexa is here");
-                  // This is our accessToken to our group's account
-                  var accessToken = "6a670d47c5ba447facf2684bd9a3c0ee";
-                  var baseUrl = "https://api.api.ai/v1/";
-                  //
-                  // $("#rec").click(function(event) {
-                  //       switchRecognition();
-                  // });
-                  startRecognition();
-                  //creating speech recognition functions
-                  var recognition;
-                  function startRecognition() {
-                    recognition_engine.stop();
-                    recognition = new webkitSpeechRecognition();
-                    recognition.onstart = function(event) {
-                      // updateRec();
-                    };
-                    recognition.onresult = function(event) {
-                      var text = "";
-                        for (var i = event.resultIndex; i < event.results.length; ++i) {
-                          text += event.results[i][0].transcript;
-                        }
-                        setInput(text);
-                      stopRecognition();
-                    };
-                    recognition.onend = function() {
-                      stopRecognition();
-                    };
-                    recognition.lang = "en-US";
-                    recognition.start();
-
-                    document.getElementById('pageListen').style.color="red";
-
-                  }
-
-                  function stopRecognition() {
-                    if (recognition) {
-                      recognition.stop();
-                      recognition_engine.start();
-                      recognition = null;
-                      document.getElementById('pageListen').style.color="#9DA1A2";
-                    }
-                    // updateRec();
-                  }
-                  function switchRecognition() {
-                    if (recognition) {
-                      stopRecognition();
-                    } else {
-                      startRecognition();
-                    }
-                  }
-                  //this just prints the utterance in the textbar
-                  function setInput(text) {
-                    action(text);
-                  }
-                  // function updateRec() {
-                  //   $("#rec").text(recognition ? "Stop" : "Speak");
-                  // }
-                  function action(text) {
-                    $.ajax({
-                      type: "POST",
-                      url: baseUrl + "query?v=20150910",
-                      contentType: "application/json; charset=utf-8",
-                      dataType: "json",
-                      headers: {
-                        "Authorization": "Bearer " + accessToken
-                      },
-                      data: JSON.stringify({ query: text, lang: "en", sessionId: "somerandomthing" }),
-                      success: function(data) {
-                        if (data.result.action=='next_step'){
-                          var current_step = Session.get("step");
-                          current_step.number = current_step.number + 1;
-                          Session.set("step", current_step);
-                          $("span[name=number] a[name=number_" + Session.get("step").number + "]").click();
-                          var current_step_instruction = Session.get("step").step;
-                            console.log("current step: "+current_step.number);
-                            console.log(current_step_instruction);
-                            responsiveVoice.speak("step"+ current_step.number + current_step_instruction, "US English Female", {pitch:1.2});
-                            //i++, write function nextStep(recipe.steps[i]) and call it here
-                        }
-                        if (data.result.action=='show_instruction'){
-                          console.log("show_instruction");
-                          $("#popup_button").click();
-                          var current_step = Session.get("step");
-                          var current_step_instruction = Session.get("step").step;
-                          responsiveVoice.speak("step"+ current_step.number + current_step_instruction, "US English Female", {pitch:1.2});
-
-                        }
-                        if (data.result.action=='repeat'){
-                          console.log('repeat');
-                          var current_step = Session.get("step");
-                          var current_step_instruction = Session.get("step").step;
-                          responsiveVoice.speak("step"+ current_step.number + current_step_instruction,  "US English Female", {pitch:1.2});
-
-                        }
-                        console.log(data);
-                      },
-                    });
-                    switchRecognition();
-                  }
-                }
               } else {
                 interim_result = result[0].transcript;
                 console.log(interim_result);
+              }
+              if(final_result.includes('stop')){
+                console.log("record stopped");
+                recognition_engine.stop();
+              }
+              if(final_result.includes('cookie')){
+                // recognition_engine.stop();
+                console.log("Alexa is here");
+                // This is our accessToken to our group's account
+                var accessToken = "6a670d47c5ba447facf2684bd9a3c0ee";
+                var baseUrl = "https://api.api.ai/v1/";
+                //
+                // $("#rec").click(function(event) {
+                //       switchRecognition();
+                // });
+                startRecognition();
+                //creating speech recognition functions
+                var recognition;
+                function startRecognition() {
+
+                  recognition = new webkitSpeechRecognition();
+                  recognition.onstart = function(event) {
+                    // updateRec();
+                  };
+                  recognition.onresult = function(event) {
+                    var text = "";
+                      for (var i = event.resultIndex; i < event.results.length; ++i) {
+                        text += event.results[i][0].transcript;
+                      }
+                      setInput(text);
+                    stopRecognition();
+                  };
+                  recognition.onend = function() {
+                    stopRecognition();
+                  };
+                  recognition.lang = "en-US";
+                  recognition.start();
+                  recognition_engine.stop();
+                  document.getElementById('pageListen').style.color="red";
+
+                }
+
+                function stopRecognition() {
+                  if (recognition) {
+                    recognition.stop();
+                    recognition_engine.start();
+                    recognition = null;
+                    document.getElementById('pageListen').style.color="#9DA1A2";
+                  }
+                  // updateRec();
+                }
+                function switchRecognition() {
+                  if (recognition) {
+                    stopRecognition();
+                  } else {
+                    startRecognition();
+                  }
+                }
+                //this just prints the utterance in the textbar
+                function setInput(text) {
+                  action(text);
+                }
+                // function updateRec() {
+                //   $("#rec").text(recognition ? "Stop" : "Speak");
+                // }
+                function action(text) {
+                  $.ajax({
+                    type: "POST",
+                    url: baseUrl + "query?v=20150910",
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    headers: {
+                      "Authorization": "Bearer " + accessToken
+                    },
+                    data: JSON.stringify({ query: text, lang: "en", sessionId: "somerandomthing" }),
+                    success: function(data) {
+                      if (data.result.action=='next_step'){
+                        var current_step = Session.get("step");
+                        current_step.number = current_step.number + 1;
+                        Session.set("step", current_step);
+                        $("span[name=number] a[name=number_" + Session.get("step").number + "]").click();
+                        var current_step_instruction = Session.get("step").step;
+                          console.log("current step: "+current_step.number);
+                          console.log(current_step_instruction);
+                          responsiveVoice.speak("step"+ current_step.number + current_step_instruction, "US English Female", {pitch:1.2});
+                          //i++, write function nextStep(recipe.steps[i]) and call it here
+                      }
+                      if (data.result.action=='show_instruction'){
+                        console.log("show_instruction");
+                        $("#popup_button").click();
+                        var current_step = Session.get("step");
+                        var current_step_instruction = Session.get("step").step;
+                        responsiveVoice.speak("step"+ current_step.number + current_step_instruction, "US English Female", {pitch:1.2});
+
+                      }
+                      if (data.result.action=='repeat'){
+                        console.log('repeat');
+                        var current_step = Session.get("step");
+                        var current_step_instruction = Session.get("step").step;
+                        responsiveVoice.speak("step"+ current_step.number + current_step_instruction,  "US English Female", {pitch:1.2});
+
+                      }
+                      console.log(data);
+                    },
+                  });
+                  switchRecognition();
+                }
               }
             }
           };
