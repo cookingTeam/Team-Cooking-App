fc3d7bf0faa96bffa49cd43ffbfaee0079d95436
ww199766 <weiweiwei@brandeis.edu>
2017-07-19 04:04:45 -0400

    Historical Improvements on our Search Engine!!

--- server/api.js
+++ server/api.js
@@ -1,10 +1,11 @@
 Meteor.methods({
-  'getRecipe': function(search_param_dish){
+  'getRecipe': function(query){
     // var new_dish = dish.split(' ').join('+');
     // console.log(new_dish);
-    const url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?instructionsRequired=true&limitLicense=false&number=10&offset=0&query="+search_param_dish+"&type=main+course";
+    const url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?"+query;
     console.log(url);
     const result = Meteor.http.call("get", url, {headers:{'X-Mashape-Key': "wwws95RenimshozMKudsHrQbarSCp1eWc80jsnt3Z3FnrU6XXb", 'Accept': "application/json"}});
+//wwws95RenimshozMKudsHrQbarSCp1eWc80jsnt3Z3FnrU6XXb
     console.log("result:"+result);
     console.log("result.content:"+result.content);
     return result.content;
