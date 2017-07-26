72182ee0b010d08e3d745599a2da20fd02e1bd55
mmarcus11 <mmarcus@brandeis.edu>
2017-07-26 11:47:32 -0400

    fixing recipe page layout

--- page/profile/client/profile.js
+++ page/profile/client/profile.js
@@ -63,27 +63,3 @@ Template.savedrow.events({
           }
       }
 })
-
-Template.personalShowRecipe.helpers({
-  recipeData() {return Myrecipe.find()}
-})
-Template.personalShowRecipe.onCreated(function(){
-  Meteor.subscribe('myrecipe');
-})
-
-Template.personalRecipeRow.helpers({
-  isOwner() {
-    console.dir(this);
-    return this.recipe.owner == Meteor.userId()}
-})
-
-Template.personalRecipeRow.events({
-  'click span'(elt,instance){
-    Meteor.call('myrecipe.remove', this.recipe);
-  },
-  'click td': function(elt, instance){
-    ownRecipeId = this.recipe._id
-    console.dir(this.recipe._id);
-    Router.go('/ownRecipePage/'+ownRecipeId);
-  }
-})
