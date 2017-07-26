729280da4094118bbee6544609c0f98030e3f099
hebuer <zhuoyuan@brandeis.edu>
2017-07-26 19:52:22 -0400

    loop over JSON

--- server/methods.js
+++ server/methods.js
@@ -4,8 +4,21 @@ var fs = require('fs');
 var path = Npm.require('path');
 var basePath = path.resolve('.').split('.meteor')[0];
 
+
 Meteor.startup(() => {
   // code to run on server at startup
+  fs.readFile('C:/Users/Yuan He/Desktop/train.json', (err, data) => {
+    if (err) throw err;
+    data = JSON.parse(data);
+    // console.log(data);
+    for (let i = 0; i < 1; i++) {
+       var d = data[i];
+       console.log(d.ingredients);
+       console.log(d.ingredients[0]);
+    }
+  });
+
+
 });
 
 Meteor.methods({
